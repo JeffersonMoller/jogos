0b09903889c4b5c8e7d34f1f6c2d7c2514ae1b3e
Jefferson Möller <infomoller@gmail.com>
2022-08-13 02:44:12 -0300

    resolver erro

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,7 +3,7 @@ total_de_tentativas = 3
 rodada = 1
 
 while rodada <= total_de_tentativas:
-    print("Tentativa {} de {} ", format(rodada, total_de_tentativas))
+    print("Tentativa {} de {} ".format(rodada, total_de_tentativas))
     chute_str = input("Digite o seu numero: ")
     print("você digitou: ", chute_str)
     chute = int(chute_str)
