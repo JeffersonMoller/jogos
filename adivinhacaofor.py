aef14d0face8c9702642767321d878f16e10edcf
Jefferson Möller <infomoller@gmail.com>
2022-08-13 19:28:22 -0300

    adicionando break e continue

--- adivinhacaofor.py
+++ adivinhacaofor.py
@@ -1,11 +1,19 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
 numero_secreto = 42
 total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {} ".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu numero: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("você digitou: ", chute_str)
     chute = int(chute_str)
+
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um numero entre 1 e 100!")
+        continue  # Volta para proxima interação
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -13,6 +21,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if acertou:
         print("Voce acertou!")
+        break
     else:
         if maior:
             print("Voce errou! seu chute foi maior do que o numero secreto.")
--- adivinhacaowhile.py
+++ adivinhacaowhile.py
@@ -1,3 +1,7 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
 numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
