1994dfee0755ccbf4ee3378de5100b8141276452
Jefferson Möller <infomoller@gmail.com>
2022-08-13 02:40:11 -0300

    formatando

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,8 +2,8 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while (rodada <= total_de_tentativas):
-    print("Tentativa: ", rodada, " de ", total_de_tentativas)
+while rodada <= total_de_tentativas:
+    print("Tentativa {} de {} ", format(rodada, total_de_tentativas))
     chute_str = input("Digite o seu numero: ")
     print("você digitou: ", chute_str)
     chute = int(chute_str)
@@ -12,16 +12,14 @@ while (rodada <= total_de_tentativas):
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
-    if(acertou):
+    if acertou:
         print("Voce acertou!")
     else:
-        if(maior):
+        if maior:
             print("Voce errou! seu chute foi maior do que o numero secreto.")
-        elif(menor):
+        elif menor:
             print("Voce errou! seu chute foi menor do que o numero secreto.")
 
     rodada = rodada + 1
 
-
     print("Fim Jogo")
-
