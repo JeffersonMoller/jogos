87841ffb7c074a1e176a81098567bf6e1769e255
Jefferson Möller <infomoller@gmail.com>
2022-08-13 01:59:08 -0300

    Primeiro exercicio

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,20 @@
-print("Olá mundo");
+numero_secreto = 42
+
+chute_str = input("Digite o seu numero: ")
+
+chute = int(chute_str)
+
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if(acertou):
+    print("Voce acertou!")
+else:
+    if(maior):
+        print("Voce errou! seu chute foi maior do que o numero secreto.")
+    elif(menor):
+        print("Voce errou! seu chute foi menor do que o numero secreto.")
+
+print("Fim Jogo")
+
