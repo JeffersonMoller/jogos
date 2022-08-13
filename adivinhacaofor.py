7ee917ba30fb06715b7948ca703ef072fe6dfb96
Jefferson Möller <infomoller@gmail.com>
2022-08-13 10:51:14 -0300

    adicionando comentários

--- adivinhacaofor.py
+++ adivinhacaofor.py
@@ -1,7 +1,6 @@
 numero_secreto = 42
 total_de_tentativas = 3
 
-
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {} ".format(rodada, total_de_tentativas))
     chute_str = input("Digite o seu numero: ")
@@ -22,5 +21,6 @@ for rodada in range(1, total_de_tentativas + 1):
 
     print("Fim Jogo")
 
-    #for rodada in range(1, 10, 2): vai de 1 até 9 pulando de 2 em 2..
-    #for rodada in [1, 2, 3, 4, 5] vai de 1 até 5
+    # for rodada in range(1, 10): vai de 1 até 9
+    # for rodada in range(1, 10, 2): vai de 1 até 9 pulando de 2 em 2..
+    # for rodada in [1, 2, 3, 4, 5]: vai de 1 até 5
