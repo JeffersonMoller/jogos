2008b74320fbc480ce6d6c68117ebfb19d9e2c6c
Jefferson Möller <infomoller@gmail.com>
2022-08-14 02:00:09 -0300

    Adicionar nível de dificuldade

--- adivinhacaowhile.py
+++ adivinhacaowhile.py
@@ -5,8 +5,20 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = round(random.randrange(1, 101))
-total_de_tentativas = 3
+total_de_tentativas = 0
 rodada = 1
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Define o nível: "))
+
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 while rodada <= total_de_tentativas:
     print("Tentativa {} de {} ".format(rodada, total_de_tentativas))
